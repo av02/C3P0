eaa7f9fd11e294712e63d85d9e265df413dbd2c1
av0207 <alex.monocanon.av@gmail.com>
2022-09-16 01:23:49 +0200

    arg positionnel oublié

--- commandes/meilleurs_trophes.py
+++ commandes/meilleurs_trophes.py
@@ -3,7 +3,7 @@ import discord
 
 async def maj_meilleurs_trophés(liste_joueurs:list,client_discord:discord.Client):
     salon = client_discord.get_channel(859386512129654794)
-    liste_joueurs.sort(lambda x:x.trophies)
+    liste_joueurs.sort(key=lambda x:x.trophies)
     message=f"liste des plus hauts trophés de l'empire:\n"
     for j in liste_joueurs[0:50]:
         message+=f"\n{j.trophies}:      {j.name}"
