33036968c16c6a5fe39b3f835fe05fc41f40a506
av0207 <alex.monocanon.av@gmail.com>
2022-09-18 00:58:50 +0200

    ajout signal

--- boucle_infinie_coc.py
+++ boucle_infinie_coc.py
@@ -16,6 +16,7 @@ async def demarage(config,connection_bdd,cocClient,discordClient):
         except coc.errors.NotFound:
             await discordClient.get_user(397116327887896576).send(f"ce tag fous la merde:{tag}")
         else:
+            print("boucle inf init")
             connection_bdd.maj_info(tag=player.tag,
                                 clan=player.clan.tag if player.clan is not None else None,
                                 pseudo=player.name,
