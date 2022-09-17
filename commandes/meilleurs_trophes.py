213de585cc3a15705f77fac7c720c1a2ff7c6cfb
av0207 <alex.monocanon.av@gmail.com>
2022-09-17 19:36:20 +0200

    ajout log

--- commandes/meilleurs_trophes.py
+++ commandes/meilleurs_trophes.py
@@ -5,7 +5,9 @@ async def maj_meilleurs_trophés(liste_joueurs:list,client_discord:discord.Clien
     salon = client_discord.get_channel(859386512129654794)
     liste_joueurs.sort(key=lambda x:x.trophies)
     message=f"liste des plus hauts trophés de l'empire:\n"
+    print("start loop")
     for j in liste_joueurs[0:50]:
+        print(f"{j.name}, ça bug")
         message+=f"\n{j.trophies}:      {j.name}"
     message+="50"+"-"*10
     for j in liste_joueurs[50:80]:
