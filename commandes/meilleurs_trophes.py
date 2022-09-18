51543a31885a90eb57d37eb8b7f6a74b7e0bb547
av0207 <alex.monocanon.av@gmail.com>
2022-09-18 05:17:57 +0200

    ordre décroissant

--- commandes/meilleurs_trophes.py
+++ commandes/meilleurs_trophes.py
@@ -3,7 +3,7 @@ import discord
 
 async def maj_meilleurs_trophés(liste_joueurs:list,client_discord:discord.Client):
     salon = client_discord.get_channel(859386512129654794)
-    liste_joueurs.sort(key=lambda x:x.trophies)
+    liste_joueurs.sort(key=lambda x:x.trophies,reverse=True)
     message=f"liste des plus hauts trophés de l'empire:\n"
     print("start loop")
     print(liste_joueurs)
