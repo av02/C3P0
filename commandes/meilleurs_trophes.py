87a3a637a70b91261f9d645100e3f07a49bd6926
av0207 <alex.monocanon.av@gmail.com>
2022-09-18 14:50:59 +0200

    ajout énumération

--- commandes/meilleurs_trophes.py
+++ commandes/meilleurs_trophes.py
@@ -4,14 +4,14 @@ import discord
 async def maj_meilleurs_trophés(liste_joueurs:list,client_discord:discord.Client):
     salon = client_discord.get_channel(859386512129654794)
     liste_joueurs.sort(key=lambda x:x.trophies,reverse=True)
-    message=f"liste des plus hauts trophés de l'empire:\n"
-    print("start loop")
+    message=f"liste des plus hauts trophés de l'empire:\n\n"
+    
     print(liste_joueurs)
-    for j in liste_joueurs[0:50]:
-        print(f"{j.name}, ça bug")
-        message+=f"\n{j.trophies}:      {j.name}"
-    message+="50"+"-"*10
-    for j in liste_joueurs[50:80]:
-        message+=f"\n{j.trophies}:      {j.name}"
+    for i,j in enumerate(liste_joueurs[0:50]):
+        
+        message+=f"\n{i+1}:{j.trophies} trophés:      {j.name}"
+    message+="\n50"+"-"*10
+    for i,j in enumerate(liste_joueurs[50:80]):
+        message+=f"\n{i+51}:{j.trophies} trophés:      {j.name}"
     await salon.send(message)
     
